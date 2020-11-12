d14c0ab0454171966b99ef39615c81286a34297f
Taiyo-Oda <noda0303@gmail.com>
2020-11-12 17:42:20 +0900

    周辺の情報を取得できるようになりました

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -17,6 +17,7 @@ function initMap() {
         // マーカーの作成
         new google.maps.Marker ({
           map: map,
+          position: mapLatLng
         })
       },
       // 取得失敗した場合
@@ -41,36 +42,6 @@ function initMap() {
   } else {
     alert("この端末では位置情報が取得できません");
   }
-  
-  // service = new google.maps.places.PlacesService(map)
-  // let getNextPage;
-  // let moreButton = document.getElementById("more");
-
-  // moreButton.onclick = function () {
-  //   // moreButtonにdisabled属性を設定（disabled属性を設定するとその値は送信されなくなる）
-  //   moreButton.disabled = true;
-
-  //   // getNextPageが可能な場合
-  //   if (getNextPage) {
-  //     getNextPage();
-  //   }
-  // }
-  // 指定した地点の周辺の情報を取得する
-  // service.nearbySearch(
-  //   { location: mapLatLng, radius: 500, type: "store" },
-  //   (results, status, pagination) => {
-  //     if (status !== "OK") return;
-  //     // 検索にヒットした施設にマーカーを設置
-  //     createMarkers(results, map);
-  //     // hasNextPageはさらに結果が利用可能かどうかを示す
-  //     moreButton.disabled = !pagination.hasNextPage;
-  //     // さらに検索結果が表示可能な場合（pagination.hasNextPageがtrueの場合）
-  //     if (pagination.hasNextPage) {
-  //       // nextPage()は次の結果セットを返す関数
-  //       getNextPage = pagination.nextPage;
-  //     }
-  //   }
-  // )
 }
 
 function codeAddress() {
@@ -88,42 +59,98 @@ function codeAddress() {
         map: map,
         position: results[0].geometry.location
       });
+      var service = new google.maps.places.PlacesService(map);
+      // let getNextPage;
+      // let moreButton = document.getElementById("more");
+      // moreButton.onclick = function () {
+      //   // moreButtonにdisabled属性を設定（disabled属性を設定するとその値は送信されなくなる）
+      //   moreButton.disabled = true;
+
+      //   // getNextPageが可能な場合
+      //   if (getNextPage) {
+      //     getNextPage();
+      //   }
+      // }
+      service.nearbySearch({
+        location: results[0].geometry.location,
+        radius: 500,
+        type: ['store']
+      }, function(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          createMarkers(results, map);
+          // hasNextPageはさらに結果が利用可能かどうかを示す
+          // moreButton.disabled = !pagination.hasNextPage;
+          // // さらに検索結果が表示可能な場合（pagination.hasNextPageがtrueの場合）
+          // if (pagination.hasNextPage) {
+          //   // nextPage()は次の結果セットを返す関数
+          //   getNextPage = pagination.nextPage;
+          // }
+        }
+      });
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
   });
 }
 
-// function createMarkers(places, map) {
-//   // LatLngBoundsクラスは境界(範囲)のインスタンスを作成する（ここでは空の境界変数を作成）
-//   const bounds = new google.maps.LatLngBounds();
-//   const placesList = document.getElementById("places");
-//   // 地図上に表示するMarkerのimageを作成
-//   for (let i = 0, place; (place = places[i]); i++) {
-//     const image = {
-//       url: place.icon,
-//       size: new google.maps.Size(71, 71),
-//       origin: new google.maps.Point(0, 0),
-//       anchor: new google.maps.Point(17, 34),
-//       scaledSize: new google.maps.Size(25, 25),
-//     }
-//     // 作成したimageを使ってマーカーを作成
-//     new google.maps.Marker({
-//       map,
-//       icon: image,
-//       title: place.name,
-//       position: place.geometry.location,
-//     })
-//     // 検索結果からli要素を作成（取得した施設のリスト）
-//     const li = document.createElement("li");
-//     li.textContent = place.name;
-//     // #placesの子要素としてliを作成
-//     placesList.appendChild(li);
-//     // 空の境界変数を取得し、すべてのマーカーを表示するlatとlngを指定
-//     bounds.extend(place.geometry.location);
+function createMarkers(places, map) {
+  // LatLngBoundsクラスは境界(範囲)のインスタンスを作成する（ここでは空の境界変数を作成）
+  const bounds = new google.maps.LatLngBounds();
+  const placesList = document.getElementById("places");
+  // 地図上に表示するMarkerのimageを作成
+  for (let i = 0, place; (place = places[i]); i++) {
+    const image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25),
+    }
+    // 作成したimageを使ってマーカーを作成
+    new google.maps.Marker({
+      map,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location,
+    })
+    // 検索結果からli要素を作成（取得した施設のリスト）
+    const li = document.createElement("li");
+    li.textContent = place.name;
+    // #placesの子要素としてliを作成
+    placesList.appendChild(li);
+    // 空の境界変数を取得し、すべてのマーカーを表示するlatとlngを指定
+    bounds.extend(place.geometry.location);
+  }
+  // mapのサイズを全てのマーカーが表示されるようにする
+  map.fitBounds(bounds);
+}
+
+// service = new google.maps.places.PlacesService(map)
+// let getNextPage;
+// let moreButton = document.getElementById("more");
+
+// moreButton.onclick = function () {
+//   // moreButtonにdisabled属性を設定（disabled属性を設定するとその値は送信されなくなる）
+//   moreButton.disabled = true;
+
+//   // getNextPageが可能な場合
+//   if (getNextPage) {
+//     getNextPage();
 //   }
-//   // mapのサイズを全てのマーカーが表示されるようにする
-//   map.fitBounds(bounds);
 // }
-
-
+// 指定した地点の周辺の情報を取得する
+// service.nearbySearch(
+//   { location: mapLatLng, radius: 500, type: "store" },
+//   (results, status, pagination) => {
+//     if (status !== "OK") return;
+//     // 検索にヒットした施設にマーカーを設置
+//     createMarkers(results, map);
+//     // hasNextPageはさらに結果が利用可能かどうかを示す
+//     moreButton.disabled = !pagination.hasNextPage;
+//     // さらに検索結果が表示可能な場合（pagination.hasNextPageがtrueの場合）
+//     if (pagination.hasNextPage) {
+//       // nextPage()は次の結果セットを返す関数
+//       getNextPage = pagination.nextPage;
+//     }
+//   }
+// )
